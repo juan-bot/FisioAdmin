0a516666ca507934a7e5089f068ff006cf52df0c
Juan Pablo Anuard Vazquez Guzman <juanpablovazques544@gmail.com>
2026-09-06 10:00:02 -0600

    improve logo

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 
 
 export default defineConfig({
   base: '/FisioAdmin/',
+  server: { port: 5175 },
   plugins: [react(), tailwindcss()],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
